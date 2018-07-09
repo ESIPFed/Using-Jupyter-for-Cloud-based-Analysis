3216558dc7e840a0cdccdb4053b39dd6b1cf2555
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-07-09 15:05:32 -0700

    changed to using properties

--- ec2/sdap-search/ingest.sh
+++ ec2/sdap-search/ingest.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+export DATA_DIR=/home/ndeploy/ingest/data/podaac-logs/
+export MUDROD_CONFIG=/home/ndeploy/Using-Jupyter-for-Cloud-based-Analysis/ec2/sdap-search/mudrod-engine.config.xml
+export SPARK_LOCAL_IP="127.0.0.1"
 
-MUDROD_CONFIG=/usr/local/sdeploy/mudrod-engine.config.xml SPARK_LOCAL_IP="127.0.0.1" mudrod-engine -f -dataDir /usr/local/sdeploy/mudrod-ingest/ftp-mirror/2016/12/ 2>&1 | tee mudrod-ingest-201612.log
+mudrod-engine -f -dataDir /usr/local/sdeploy/mudrod-ingest/ftp-mirror/2016/12/
