4ac9fd27f47c546671b718fe56b25cb64e5d0a5b
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-07-09 09:44:18 -0700

    ingestion script

---
+++ ec2/ingest/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export DATA_DIRECTORY=/home/ndeploy/ingest/data/avhrr
+export NINGESTER_CONFIG=/home/ndeploy/Using-Jupyter-for-Cloud-based-Analysis/ec2/ingest
+export VERSION=1.0.0-SNAPSHOT
+for g in `ls ${DATA_DIRECTORY} | grep "2016" | awk "{print $1}"`
+do
+  docker run -d --name $(echo avhrr_$g | cut -d'-' -f 1) --network nexus -v ${NINGESTER_CONFIG}:/config/ -v ${DATA_DIRECTORY}/${g}:/data/${g} sdap/ningester:${VERSION} docker,solr,cassandra
+  sleep 30
+done
