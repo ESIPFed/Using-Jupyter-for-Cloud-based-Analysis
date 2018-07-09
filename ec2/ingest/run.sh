e5fdc8ee4558b9af0159f3881c0b5225c6b3fd4c
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-07-09 09:46:30 -0700

    remove container when it's done

--- ec2/ingest/run.sh
+++ ec2/ingest/run.sh
@@ -5,6 +5,6 @@ export NINGESTER_CONFIG=/home/ndeploy/Using-Jupyter-for-Cloud-based-Analysis/ec2
 export VERSION=1.0.0-SNAPSHOT
 for g in `ls ${DATA_DIRECTORY} | grep "2016" | awk "{print $1}"`
 do
-  docker run -d --name $(echo avhrr_$g | cut -d'-' -f 1) --network docker_nexus -v ${NINGESTER_CONFIG}:/config/ -v ${DATA_DIRECTORY}/${g}:/data/${g} sdap/ningester:${VERSION} docker,solr,cassandra
+  docker run -d --rm --name $(echo avhrr_$g | cut -d'-' -f 1) --network docker_nexus -v ${NINGESTER_CONFIG}:/config/ -v ${DATA_DIRECTORY}/${g}:/data/${g} sdap/ningester:${VERSION} docker,solr,cassandra
   sleep 30
 done
