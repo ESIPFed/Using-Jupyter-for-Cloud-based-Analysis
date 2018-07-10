7c38030574a59640307e688b26b58e650cd6b00b
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-07-10 10:18:56 -0700

    remove download, it got complicated. update properties.

--- ec2/sdap-search/download-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p /home/ndeploy/ingest/data/podaac-logs
-cd /home/ndeploy/ingest/data/podaac-logs
-wget -m -np -nH --cut-dirs=4 ftp://podaac.jpl.nasa.gov/misc/outgoing/cjf/mudrod/2018
--- ec2/sdap-search/ingest.sh
+++ ec2/sdap-search/ingest.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 export DATA_DIR=/home/ndeploy/ingest/data/podaac-logs/
-export MUDROD_CONFIG=/home/ndeploy/Using-Jupyter-for-Cloud-based-Analysis/ec2/sdap-search/mudrod-engine.config.xml
+export MUDROD_CONFIG=/home/ndeploy/Using-Jupyter-for-Cloud-based-Analysis/ec2/sdap-search/config.properties
 export SPARK_LOCAL_IP="127.0.0.1"
 
-mudrod-engine -f -dataDir /usr/local/sdeploy/mudrod-ingest/ftp-mirror/2016/12/
+cd /home/ndeploy/sdap-search
+./bin/mudrod-engine -f -dataDir ${DATA_DIR}/2018/01
