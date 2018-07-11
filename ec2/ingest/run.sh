e5372262c0161286a52cfb25f6c1cdba89fdc88b
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-07-10 21:19:54 -0700

    added a couple more configs.

--- ec2/ingest/run.sh
+++ ec2/ingest/run.sh
@@ -3,7 +3,7 @@
 export DATA_DIRECTORY=/home/ndeploy/ingest/data/avhrr
 export NINGESTER_CONFIG=/home/ndeploy/Using-Jupyter-for-Cloud-based-Analysis/ec2/ingest
 export VERSION=1.0.0-SNAPSHOT
-for g in `ls ${DATA_DIRECTORY} | grep "2016" | awk "{print $1}"`
+for g in `ls ${DATA_DIRECTORY} | awk "{print $1}"`
 do
   docker run -d --rm --name $(echo avhrr_$g | cut -d'-' -f 1) --network docker_nexus -v ${NINGESTER_CONFIG}:/config/ -v ${DATA_DIRECTORY}/${g}:/data/${g} sdap/ningester:${VERSION} docker,solr,cassandra
   sleep 30
