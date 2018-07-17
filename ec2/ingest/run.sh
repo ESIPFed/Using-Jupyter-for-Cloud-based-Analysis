e9bbb6daa0c93f0320e36028d6d9a5237629b648
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-07-17 01:35:02 -0700

    avhrr clim

--- ec2/ingest/run.sh
+++ ec2/ingest/run.sh
@@ -9,6 +9,13 @@ do
   sleep 30
 done
 
+export DATA_DIRECTORY=/home/ndeploy/ingest/data/avhrr-clim
+for g in `ls ${DATA_DIRECTORY} | awk "{print $1}"`
+do
+  docker run -d --rm --name $(echo avhrr-clim_$g | cut -d'.' -f 1) --network docker_nexus -v ${NINGESTER_CONFIG}/connections.yml:/config/connections.yml -v ${NINGESTER_CONFIG}/avhrr-clim.yml:/config/avhrr-clim.yml -v ${DATA_DIRECTORY}/${g}:/data/${g} sdap/ningester:${VERSION} docker,solr,cassandra
+  sleep 5
+done
+
 export DATA_DIRECTORY=/home/ndeploy/ingest/data/trmm
 for g in `ls ${DATA_DIRECTORY} | egrep '1998' | awk "{print $1}"`
 do
