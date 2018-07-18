7b39b622c46cda8b71e989e33935aa9d555c46d3
Frank Greguska <francis.greguska@jpl.nasa.gov>
2018-07-18 02:34:11 -0700

    copy script

---
+++ notebooks/copy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+mkdir -p ~/Using-Jupyter-for-Cloud-based-Analysis
+cp /home/jovyan/shared/2018_summer_workshops/Using-Jupyter-for-Cloud-based-Analysis/notebooks/* ~/Using-Jupyter-for-Cloud-based-Analysis
